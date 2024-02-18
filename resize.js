aaf7ef5e6940103b0e45255a58122fac12e0ccce
Sarah Duban <sarahgrut@hotmail.fr>
2024-02-18 15:45:16 +0100

    css du titre retiré de resize.js et placé dans accueil.js

--- resize.js
+++ resize.js
@@ -1,8 +1,5 @@
 window.addEventListener("load",function (){
-    var sousTitre=document.querySelector("h1");
-    var titre=document.getElementById('titre');
-    var bienvenue=document.getElementById('Bienvenue');
-    var containerMurliimg=document.getElementById("containerMur li img");
+   var containerMurliimg=document.getElementById("containerMur li img");
     var containerMur2liimg=document.getElementById("containerMur2 li img");
     var containerMur3liimg=document.getElementById("containerMur3 li img");
     
@@ -10,9 +7,6 @@ window.addEventListener("load",function (){
    
     if (window.matchMedia("(max-width:480px)").matches)
     {
-      titre.style.fontSize="35px",
-      bienvenue.style.fontSize="35px",
-      sousTitre.style.fontSize="20px",
       containerMurliimg.style.marginTop="-20px",
       containerMur2liimg.style.marginTop="-20px",
       containerMur3liimg.style.marginTop="-20px"
